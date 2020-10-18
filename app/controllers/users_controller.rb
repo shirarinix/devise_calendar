8ab20c0a3b8c2595e3db1b8a3b59016dc6761478
shirarinix <yukatoyoshirin@gmail.com>
2020-10-18 17:53:34 +0900

    Revert "編集失敗（取り消し）"
    This reverts commit 787e0c5fd4035522cf0651e02812b03430765929.

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-  def show
-    @user = User.find(params[:id])
-    @artists = @user.artists
-
-    users_artists = UserArtist.where(user_id: current_user.id).pluck(:artist_id) # ログイン中のユーザーのお気に入りのartist_idカラムを取得
-    @user_artist_list = Artist.find(users_artists) # artistsテーブルから、お気に入り登録済みのレコードを取得
-  end
-end
