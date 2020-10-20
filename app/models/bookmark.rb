8ebf014d6eefbd858a24d56c26b8fe8dcaa5c793
shirarinix <yukatoyoshirin@gmail.com>
2020-10-20 21:03:58 +0900

    アソシエーションの設定

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,7 @@
 class Bookmark < ApplicationRecord
+  belongs_to :user
+  belongs_to :artist
+
+  validates :user_id, uniqueness: { scope: :artist_id } # 一意性制限
+  # validates :artist_id, uniqueness: { scope: :user_id }
 end
