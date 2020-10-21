8923254dc715c1241f0d60574325b0d1e66ee8a3
shirarinix <yukatoyoshirin@gmail.com>
2020-10-21 20:29:07 +0900

    モデルのアソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :artists, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookmarks_artists, through: :bookmarks, source: :artist
 end
