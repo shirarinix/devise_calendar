c92783675b838b6c8b7af270ee2418283bb6b456
shirarinix <yukatoyoshirin@gmail.com>
2021-01-09 19:09:46 +0900

    フォロー機能のvalidationテスト実装

---
+++ spec/models/bookmark_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Bookmark, type: :model do
+  describe 'フォロー機能 validationテスト' do
+    it { should validate_uniqueness_of(:following_id).scoped_to(:follower_id, :follow_id, :followed_id).with_message("エラー発生") }
+  end
+end
