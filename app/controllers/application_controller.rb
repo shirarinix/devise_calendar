ca45b365a3e221dcea973214811895f54191b539
shirarinix <yukatoyoshirin@gmail.com>
2021-05-06 03:13:48 +0900

    artistプロフィール編集機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,22 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']  # 環境変数を読み込む記述に変更
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD'] # 環境変数を読み込む記述に変更
     end
   end
 
   protected
 
   def configuer_permitted_parameters
+    # ユーザーのサインアップ
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :user_image, :sex, :hobby, :birthday])
+    # ユーザーのアカウントアップデート
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :user_image, :sex, :hobby, :birthday])
+    # アーティストのサインアップ
     devise_parameter_sanitizer.permit(:sign_up, keys: [:artistname, :artist_image, :sex, :hobby, :birthday,
+                                                       :discography, :twitter, :facebook, :instagram])
+    # アーティストのアカウントアップデート
+    devise_parameter_sanitizer.permit(:account_update, keys: [:artistname, :artist_image, :sex, :hobby, :birthday,
                                                        :discography, :twitter, :facebook, :instagram])
   end
 end
